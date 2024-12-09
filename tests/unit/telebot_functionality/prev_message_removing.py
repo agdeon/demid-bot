e37d864292903623c7cbd0de99f54ff8cf874dc9
Drew <37987027+agdeon@users.noreply.github.com>
2024-12-09 00:02:34 +0000

    some tests

---
+++ tests/unit/telebot_functionality/prev_message_removing.py
@@ -0,0 +1,25 @@
+import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+import os
+import time
+
+from dotenv import load_dotenv
+
+load_dotenv()
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+bot = telebot.TeleBot(BOT_TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    user_message_id = message.message_id
+    sent_message = bot.send_message(message.chat.id, "Это первое сообщение.")
+    time.sleep(1)
+    bot.delete_message(message.chat.id, sent_message.message_id)
+    bot.delete_message(message.chat.id, user_message_id)
+    sent_message = bot.send_message(message.chat.id, "Это новое сообщение!")
+    time.sleep(1)
+    bot.delete_message(message.chat.id, sent_message.message_id)
+
+
+bot.infinity_polling()
